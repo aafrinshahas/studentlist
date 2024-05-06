5551eccfa8f68878df322bc51fc59f904f0e0672
aafrinshahas <codergit6@gmail.com>
2024-05-06 12:22:03 +0530

    update disable button functionality

--- src/Studentlist.jsx
+++ src/Studentlist.jsx
@@ -1,4 +1,3 @@
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 import { useState } from "react";
 
 function Studentlist(props){
@@ -11,43 +10,46 @@ const [buttonDisable, setbuttonDisable] = useState(false)
 function handleAdd(selectedId){
 console.log(selectedId)
 const temp = sList.filter((item)=>{
-  return item.id == selectedId
+return item.id == selectedId
 })
-// if(buttonDisable === false){
-//   setbuttonDisable(!buttonDisable)
-//   }
+
 console.log(temp)
 const selectedItem = temp.map((item)=>{
-  return item.name
+return item.name
 })
+
 
 console.log(selectedItem)
 setfList([...fList, selectedItem])
-
+setbuttonDisable(true)
 }
 
-    return(
+// function disableCount(event){
+//   event.preventDefault();
+//   setbuttonDisable(true)
+// }
 
-        <div className="p-5">
-              <h1 className="text-xl">Student List</h1>
-              <ul className="border mt-4 p-2" style={{width: '35%'}}>
-              {
-                sList.map((item, index)=>{
-                   return(
-                    <li key={index} className="py-3 flex justify-between">{index+1}. {item.name} 
-                  <button 
-                  disabled={buttonDisable}
-                  className="bg-black text-white px-2 py-1 rounded-md" 
-                     onClick={()=>handleAdd(item.id)}>Add to Favourite</button>
-                    </li>
-                   )
-                })
-              }
-              </ul>
-              
-        </div>
-       
+return(
+
+<div className="p-5">
+<h1 className="text-xl">Student List</h1>
+<ul className="border mt-4 p-2" style={{width: '35%'}}>
+{
+sList.map((item, index)=>{
+    return(
+    <li key={index} className="py-3 flex justify-between">{index+1}. {item.name} 
+  <button 
+  className="bg-black text-white px-2 py-1 rounded-md"
+      onClick={buttonDisable ? ()=>{} : ()=>handleAdd(item.id)}>Add to Favourite</button>
+    </li>
     )
+})
+}
+</ul>
+
+</div>
+
+)
 }
 
 export default Studentlist;
